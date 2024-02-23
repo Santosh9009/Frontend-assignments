e96f901aaeaf8a29d8688c226c643aabb9673454
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-02-23 05:32:03 +0530

    useCallback assignments

---
+++ frontend-assignment/src/Assignments/useCallback/Assignment1.jsx
@@ -0,0 +1,38 @@
+import { useCallback } from "react";
+import { useState } from "react";
+
+// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+
+export function Assignment1() {
+    const [count, setCount] = useState(0);
+
+    
+    // Your code starts here
+    const handleIncrement=useCallback(()=> {
+        setCount(count+1);
+        console.log('tatti')
+    },[count])
+
+    const handleDecrement=useCallback(()=> {
+        setCount(count-1);
+    },[count])
+
+   
+    // Your code ends here
+
+    return (
+        <div>
+            <div>hi there</div>
+            <p>Count: {count}</p>
+            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
+        </div>
+    );
+};
+
+// eslint-disable-next-line react/prop-types
+const CounterButtons = ({ onIncrement, onDecrement }) => (
+    <div>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+    </div>
+);
